53ee87e84bdd20778cce1ed7dfe0e430dc0d1259
ArjunRai1 <arjunrai2214@gmail.com>
2025-11-28 09:15:48 +0530

    Making API endpoints

---
+++ DVC_Pipeline/src/main.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from fastapi import FastAPI
+import pickle
+from watermodel import Water
+app = FastAPI(
+    title="Water Potability Prediction",
+    description="Predicting if water is fit for consumption or not"
+)
+
+@app.get("/")
+def index():
+    return "Welcome to Water Potability Prediction"
+
+@app.post("/water-potability-pred")
+def predict(Water: Water):
+    sample = pd.DataFrame({
+    "ph": [Water.ph],
+    "Hardness": [Water.Hardness],
+    "Solids": [Water.Solids],
+    "Chloramines": [Water.Chloramines],
+    "Sulfate": [Water.Sulfate],
+    "Conductivity": [Water.Conductivity],
+    "Organic_carbon": [Water.Organic_carbon],
+    "Trihalomethanes": [Water.Trihalomethanes],
+    "Turbidity": [Water.Turbidity],
+    })
+
+    model = pickle.load(open("C:\\Users\\arjun\\Documents\\MLOps\\DVC_Pipeline\\model.pkl", "rb"))
+    prediction = model.predict(sample)
+
+    if prediction == 1:
+        return "Water is fit for consumption"
+    else:
+        return "Water is not fit for consumption"
