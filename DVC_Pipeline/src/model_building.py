363cc5ec8a56aa5095a5c9dabef256cae585c189
ArjunRai1 <arjunrai2214@gmail.com>
2025-11-29 10:06:57 +0530

    Hyperparameter tuning

--- DVC_Pipeline/src/model_building.py
+++ DVC_Pipeline/src/model_building.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import pickle
 from sklearn.ensemble import RandomForestClassifier
+import yaml
 
 train_data = pd.read_csv("./data/processed/train_preprocessed.csv")
 
 X_train = train_data.drop(columns=['Potability'], axis=1)
 y_train = train_data['Potability']
 
-rf = RandomForestClassifier(n_estimators=50, max_depth=5, min_samples_leaf=5, random_state=42)
+n_estimators = yaml.safe_load(open("params.yaml", "r"))["model_building"]["n_estimators"]
+min_samples_leaf = yaml.safe_load(open("params.yaml", "r"))["model_building"]["min_samples_leaf"]
+max_depth = yaml.safe_load(open("params.yaml", "r"))["model_building"]["max_depth"]
+
+rf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, min_samples_leaf=min_samples_leaf, random_state=42)
 
 rf.fit(X_train, y_train)
 
