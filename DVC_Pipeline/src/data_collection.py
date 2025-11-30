2b6f4823b6b57a8ab81a726e4681c83f14a4bc27
ArjunRai1 <arjunrai2214@gmail.com>
2025-11-30 12:37:34 +0530

    Added exception handling

--- DVC_Pipeline/src/data_collection.py
+++ DVC_Pipeline/src/data_collection.py
@@ -6,34 +6,51 @@ import yaml
 
 # data = pd.read_csv(r"C:\\Users\\arjun\\Downloads\\water_potability.csv")
 def load_data(filepath: str)->pd.DataFrame:
-    return pd.read_csv(filepath)
+    try:
+        return pd.read_csv(filepath)
+    except Exception as e:
+        raise Exception(f"Error loading data from {filepath}: {e}")
 
 # test_size = yaml.safe_load(open("params.yaml", "r"))["data_collection"]["test_size"]
 def load_params(filepath: str)->float:
-    with open(filepath, 'r') as file:
-        params = yaml.safe_load(file)["data_collection"]["test_size"]
-    return params
+    try:
+        with open(filepath, 'r') as file:
+            params = yaml.safe_load(file)
+            return params["data_collection"]["test_size"]
+    except Exception as e:
+        raise Exception(f"Error loading params from {filepath}: {e}")
+    
 
 # train_data, test_data = train_test_split(data, test_size=test_size, random_state=42)
-def split(data: pd.DataFrame, test_size:float)->tuple[pd.DataFrame, pd.DataFrame]:
-    return train_test_split(data, test_size=test_size, random_state=42)
+def split_data(data: pd.DataFrame, test_size:float)->tuple[pd.DataFrame, pd.DataFrame]:
+    try:
+        return train_test_split(data, test_size=test_size, random_state=42)
+    except ValueError as e:
+        raise ValueError(f"Error splitting data into train and test: {e}")
 
-def save_csv(df:pd.DataFrame, filepath:str)->None:
-    df.to_csv(filepath, index=False) 
-
+def save_data(df:pd.DataFrame, filepath:str)->None:
+    try:
+        df.to_csv(filepath, index=False) 
+    except Exception as e:
+        raise Exception(f"Error saving data to {filepath}: {e}")
 #data_path = os.path.join("data", "raw")
 
 def main():
     data_filepath = r"C:\\Users\\arjun\\Downloads\\water_potability.csv"
     params_filepath = "params.yaml"
-    raw_data_path = os.path.join("data", "raw")
-    data = load_data(data_filepath)
-    test_size = load_params(params_filepath)
-    train_data, test_data = split(data, test_size)
-    os.makedirs(raw_data_path)
+    raw_data_path = os.path.join("data","raw")
+# data_path = os.path.join("data","raw")
+    try:
+        data = load_data(data_filepath)
+        test_size = load_params(params_filepath)
+        train_data,test_data = split_data(data, test_size)
 
-    save_csv(os.path.join(raw_data_path, "train.csv"))
-    save_csv(os.path.join(raw_data_path, "test.csv"))
+        os.makedirs(raw_data_path)
 
-if __name__ == "main":
+        save_data(train_data,os.path.join(raw_data_path,"train.csv"))
+        save_data(test_data, os.path.join(raw_data_path,"test.csv"))
+    except Exception as e:
+        raise Exception(f"An error occurred :{e}")
+    
+if __name__ == "__main__":
     main()
--- DVC_Pipeline/src/data_preprocessing.py
+++ DVC_Pipeline/src/data_preprocessing.py
@@ -2,21 +2,47 @@ import numpy as np
 import pandas as pd
 import os
 
-train_data = pd.read_csv("./data\\raw\\train.csv")
-test_data = pd.read_csv("./data\\raw\\test.csv")
+# train_data = pd.read_csv("./data\\raw\\train.csv")
+# test_data = pd.read_csv("./data\\raw\\test.csv")
+def load_data(filepath: str)->pd.DataFrame:
+    try:
+        return pd.read_csv(filepath)
+    except Exception as e:
+        return Exception(f"Failed to load data from {filepath}: {e}")
 
 def simple_preprocess(df):
-    for column in df.columns:
-        median = df[column].median()
-        df[column].fillna(median, inplace=True)
-    return df
+    try:
+        for column in df.columns:
+            median = df[column].median()
+            df[column].fillna(median, inplace=True)
+        return df
+    except Exception as e:
+        return Exception(f"Failed to perform pre-processing: {e}")
 
-train_preprocess = simple_preprocess(train_data)
-test_preprocess = simple_preprocess(test_data)
+# train_preprocess = simple_preprocess(train_data)
+# test_preprocess = simple_preprocess(test_data)
+def save_data(df:pd.DataFrame, filepath:str)->None:
+    try:
+        df.to_csv(filepath, index=False) 
+    except Exception as e:
+        raise Exception(f"Error saving data to {filepath}: {e}")
 
-data_path = os.path.join("data", "processed")
+def main():
+    try:
+        raw_data_path = "./data/raw"
+        processed_data_path = "./data/processed"
 
-os.makedirs(data_path)
+        train_data = load_data(os.path.join(raw_data_path, "train.csv"))
+        test_data = load_data(os.path.join(raw_data_path, "test.csv"))
 
-train_preprocess.to_csv(os.path.join(data_path, "train_preprocessed.csv"), index=False)
-test_preprocess.to_csv(os.path.join(data_path, "test_preprocessed.csv"), index=False)
+        train_processed_data = simple_preprocess(train_data)
+        test_processed_data = simple_preprocess(test_data)
+
+        os.makedirs(processed_data_path)
+
+        save_data(train_processed_data, os.path.join(processed_data_path, "train_preprocessed.csv"))
+        save_data(test_processed_data, os.path.join(processed_data_path, "test_preprocessed.csv"))
+    except Exception as e:
+        raise Exception(f"Error occurred: {e}")
+if __name__ == "__main__":
+    main()
